e125bec026e010dbd505acc4eeb8b25f1611345e
ap <princehans2023@gmail.com>
2023-09-30 09:41:52 +0630

    finished routes

---
+++ app/controllers/routes.go
@@ -0,0 +1,34 @@
+package controllers
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func (server *Server) initializeRoutes() {
+	server.Router = mux.NewRouter()
+	server.Router.HandleFunc("/", server.Home).Methods("GET")
+
+	server.Router.HandleFunc("/login", server.Login).Methods("GET")
+	server.Router.HandleFunc("/login", server.DoLogin).Methods("POST")
+	server.Router.HandleFunc("/register", server.Register).Methods("GET")
+	server.Router.HandleFunc("/register", server.DoRegister).Methods("POST")
+	server.Router.HandleFunc("/logout", server.Logout).Methods("GET")
+
+	server.Router.HandleFunc("/products", server.Products).Methods("GET")
+	server.Router.HandleFunc("/products/{slug}", server.GetProductBySlug).Methods("GET")
+
+	server.Router.HandleFunc("/carts", server.GetCart).Methods("GET")
+	server.Router.HandleFunc("/carts", server.AddItemToCart).Methods("POST")
+	server.Router.HandleFunc("/carts/update", server.UpdateCart).Methods("POST")
+	server.Router.HandleFunc("/carts/cities", server.GetCitiesByProvince).Methods("GET")
+	server.Router.HandleFunc("/carts/calculate-shipping", server.CalculateShipping).Methods("POST")
+	server.Router.HandleFunc("/carts/apply-shipping", server.ApplyShipping).Methods("POST")
+	server.Router.HandleFunc("/carts/remove/{id}", server.RemoveItemByID).Methods("GET")
+
+	server.Router.HandleFunc("/orders/checkout", server.Checkout).Methods("POST")
+
+	
+	server.Router.PathPrefix("/public/").Handler(staticFileHandler).Methods("GET")
+}
